dc9a4c915c0123ebf159fc2623b9d787fe403952
Bram Vogelaar <bram@attachmentgenie.com>
2022-11-25 01:57:57 +0100

    chore: swithching oncall to connect services
    Signed-off-by: Bram Vogelaar <bram@attachmentgenie.com>

---
+++ terraform/grafana-intentions.tf
@@ -0,0 +1,13 @@
+resource "consul_config_entry" "oncall-dashboard" {
+  name = data.consul_service.grafana.name
+  kind = "service-intentions"
+
+  config_json = jsonencode({
+    Sources = [{
+      Action     = "allow"
+      Name       = data.consul_service.oncall.name
+      Precedence = 9
+      Type       = "consul"
+    }]
+  })
+}
